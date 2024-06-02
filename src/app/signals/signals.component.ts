937d58c64c40516efc84b37ad49ab7982625182d
brenward <bren.ward@gmail.com>
2024-06-02 13:59:41 +0100

    #Using setter method instead of update

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -12,7 +12,8 @@ export class SignalsComponent {
   counter = signal(0);
 
   increment() {
-    this.counter.update((oldcounter) => oldcounter + 1);
+    //this.counter.update((oldcounter) => oldcounter + 1);
+    this.counter.set(this.counter() + 1);
     this.actions.push('INCREMENT');
   }
 
